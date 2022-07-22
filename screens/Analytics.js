85918275802028280cee19fdb8c0dc50d0da44a3
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2022-07-22 19:09:06 +0530

    Implemented analytics using realm-aggregate

--- screens/Analytics.js
+++ screens/Analytics.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useCallback, useMemo} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useQuery} from '../contexts/RealmContext';
+import {aggregate} from 'realm-aggregate';
 
 const Analytics = () => {
+  // const realm = useRealm();
+  const expenses = useQuery('expense');
+
+  const groupExpensesOnBasisOfModes = useCallback(() => {
+    return aggregate(
+      [
+        {$group: {_id: '$mode', totalAmount: {$sum: '$amount'}}},
+        {$project: {mode: '$_id', totalAmount: '$totalAmount'}},
+      ],
+      expenses,
+    );
+  }, [expenses]);
+
+  const modeAnalytics = useMemo(() => {
+    return groupExpensesOnBasisOfModes();
+    // modesData=
+    // [
+    //   {mode: 'Cash', totalAmount: 320},
+    //   {mode: 'Credit Card', totalAmount: 1280},
+    //   {mode: 'Axis Savings A/c', totalAmount: 900},
+    //   {mode: 'UPI', totalAmount: 1390},
+    //   {mode: 'Demo', totalAmount: 2},
+    //   {mode: 'Axis CC', totalAmount: 1200},
+    //   {mode: 'Cheque', totalAmount: 3000},
+    // ];
+  }, [groupExpensesOnBasisOfModes]);
+
+  const ModeData = ({mode, totalAmount}) => {
+    return (
+      <View style={styles.modeData}>
+        <Text>
+          {mode}: {totalAmount}
+        </Text>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={[styles.container, styles.horizontal]}>
-      <Text style={styles.screenTitle}>Analytics</Text>
+      <View>
+        <Text style={styles.screenTitle}>Analytics</Text>
+      </View>
+      <FlatList
+        data={modeAnalytics}
+        renderItem={({item: {mode, totalAmount}}) => {
+          return <ModeData mode={mode} totalAmount={totalAmount} key={mode} />;
+        }}
+      />
     </SafeAreaView>
   );
 };
@@ -13,7 +60,7 @@ const Analytics = () => {
 const styles = StyleSheet.create({
   screenTitle: {
     fontSize: 40,
-    padding: 20,
+    padding: 10,
     fontWeight: 'bold',
   },
   container: {
@@ -21,8 +68,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   horizontal: {
-    flexDirection: 'row',
     justifyContent: 'space-around',
+    padding: 10,
+  },
+  modeData: {
     padding: 10,
   },
 });
