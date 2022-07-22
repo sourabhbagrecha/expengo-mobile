e6e8aed2dc4847a6a2f7d3cb1df741fd07f6c8b8
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2022-07-22 18:41:22 +0530

    Manual Analytics logic implemented

--- screens/Analytics.js
+++ screens/Analytics.js
@@ -1,11 +1,69 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useCallback, useMemo} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useQuery} from '../contexts/RealmContext';
 
 const Analytics = () => {
+  const expenses = useQuery('expense');
+
+  const groupExpensesOnBasisOfModes = useCallback(() => {
+    const resultsObj = {};
+    for (let expense of expenses) {
+      const expenseMode = expense.mode;
+      const expenseAmount = expense.amount;
+      const objectKeys = Object.keys(resultsObj);
+      const modeExistsInResults = objectKeys.includes(expenseMode);
+      if (modeExistsInResults) {
+        const currentResult = resultsObj[expenseMode];
+        currentResult.totalAmount += expenseAmount;
+      } else {
+        resultsObj[expenseMode] = {
+          totalAmount: expenseAmount,
+        };
+      }
+    }
+    const results = [];
+    for (let key in resultsObj) {
+      results.push({mode: key, ...resultsObj[key]});
+    }
+    return results;
+  }, [expenses]);
+
+  const modeAnalytics = useMemo(() => {
+    return groupExpensesOnBasisOfModes();
+    // modesData=
+    // [
+    //   {mode: 'Cash', totalAmount: 320},
+    //   {mode: 'Credit Card', totalAmount: 1280},
+    //   {mode: 'Axis Savings A/c', totalAmount: 900},
+    //   {mode: 'UPI', totalAmount: 1390},
+    //   {mode: 'Demo', totalAmount: 2},
+    //   {mode: 'Axis CC', totalAmount: 1200},
+    //   {mode: 'Cheque', totalAmount: 3000},
+    // ];
+  }, [groupExpensesOnBasisOfModes]);
+
+  const ModeData = ({mode, totalAmount}) => {
+    return (
+      <View style={styles.modeData}>
+        <Text>
+          {mode}: {totalAmount}
+        </Text>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={[styles.container, styles.horizontal]}>
-      <Text style={styles.screenTitle}>Analytics</Text>
+      <View>
+        <Text style={styles.screenTitle}>Analytics</Text>
+      </View>
+      <FlatList
+        data={modeAnalytics}
+        renderItem={({item: {mode, totalAmount}}) => {
+          return <ModeData mode={mode} totalAmount={totalAmount} key={mode} />;
+        }}
+      />
     </SafeAreaView>
   );
 };
@@ -13,7 +71,7 @@ const Analytics = () => {
 const styles = StyleSheet.create({
   screenTitle: {
     fontSize: 40,
-    padding: 20,
+    padding: 10,
     fontWeight: 'bold',
   },
   container: {
@@ -21,8 +79,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   horizontal: {
-    flexDirection: 'row',
     justifyContent: 'space-around',
+    padding: 10,
+  },
+  modeData: {
     padding: 10,
   },
 });
